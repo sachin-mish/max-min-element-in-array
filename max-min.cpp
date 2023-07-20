622dd442046d2eab4c9965c210c69f94284776b6
Sachin mishra <76901807+sachin-mish@users.noreply.github.com>
2023-07-20 14:05:25 +0530

    Add files via upload

---
+++ max-min.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include<climits>
+
+using namespace std;
+
+
+
+int main() {
+ 
+   int n;
+   
+ cin>>n;
+   
+ int arr[n];
+  
+  for(int i=0;i<n;i++){
+    
+    cin>>arr[i];
+ 
+   }
+    
+int maxn=INT_MIN;
+    
+int minn=INT_MAX;
+ 
+   
+for(int i=0;i<n;i++){
+  
+  
+  maxn=max(maxn,arr[i]);
+  
+    minn=min(minn,arr[i]);
+ 
+
+} 
+cout<<maxn<<" "<<minn;
+
+ 
+
+   return 0;
+
+}
